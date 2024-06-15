64482d4bdb0e7b23d2e4f0a6397b2b7c58cf1a2f
unknown <martinperez9713@gmail.com>
2024-06-15 13:42:28 -0300

    primera parte: consumo de apis

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,29 +1,42 @@
-import { obtenerProductos } from "../../data/data.js";
 import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
+import { getProducts, getProductById, addProduct, modProduct } from "../../utils/fetchApi.js";
 
-import "./ItemListContainer.css"
+import "./ItemListContainer.css";
 
 const ItemListContainer = ({ saludo }) => {
   const [productos, setProductos] = useState([]);
 
+  const newProduct = {
+    nombre: "Producto 3",
+    descripcion: "Descripcion 3",
+    stock: 2,
+    categoria: "mascotas",
+    imagen: "https://th.bing.com/th/id/OIP.4SsKFAsz_O3JesFuyVcFUwHaHa?rs=1&pid=ImgDetMain",
+    precio: 300,
+  };
+
+  const mod = {
+    nombre: "Alimento para perro",
+    precio: 5000
+  }
+
   useEffect(() => {
-    obtenerProductos()
-      .then((respuesta) => {
-        setProductos(respuesta);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log("finalizo la promesa");
-      });
+    getProducts().then((data) => setProductos(data));
+
+    getProductById(1).then((data) => console.log(data));
   }, []);
+
+  const clickEvento = () => {
+    //addProduct(newProduct).then((data)=> setProductos(data))
+    modProduct(1, mod).then((data)=> setProductos(data))
+  }
 
   return (
     <div className="itemlistcontainer">
       <p>{saludo}</p>
-      <ItemList productos = {productos} />
+      <button onClick={clickEvento}>Click me!</button>
+      <ItemList productos={productos} />
     </div>
   );
 };
---
+++ src/utils/fetchApi.js
@@ -0,0 +1,66 @@
+//traer todos los productos
+const getProducts = () => {
+  return fetch("http://localhost:8080/api/productos")
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error))
+}
+
+const getProductById = (idProduct) => {
+  return fetch("http://localhost:8080/api/productos/" + idProduct)
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error))
+}
+
+const addProduct = (newProduct) => {
+  const newProductJson = JSON.stringify(newProduct)
+
+  return fetch("http://localhost:8080/api/productos", {
+    method: "POST",
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: newProductJson
+  })
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error))
+}
+
+const modProduct = (idProduct, mod) => {
+  const modJson = JSON.stringify(mod)
+
+  return fetch("http://localhost:8080/api/productos/"+idProduct, {
+    method: "PUT",
+    headers: {
+      'Content-Type': "application/json"
+    },
+    body: modJson
+  })
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error))
+}
+
+const deleteProduct = (idProduct) => {
+
+  return fetch("http://localhost:8080/api/productos/"+idProduct, {
+    method: "DELETE"
+  })
+    .then((respuesta) => respuesta.json())
+    .then((data) => {
+      return data
+    })
+    .catch((error) => console.log(error))
+}
+
+export { getProducts, getProductById, addProduct, modProduct, deleteProduct }
